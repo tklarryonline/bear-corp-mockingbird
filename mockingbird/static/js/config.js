9510979fd653464d564a75922188c9652a4308e7
Lim H <limdauto@gmail.com>
2014-08-01 17:40:49 +0700

    Finish merging client & server 1

---
+++ mockingbird/static/js/config.js
@@ -0,0 +1,27 @@
+requirejs.config({
+    paths: {
+        'angular': '/static/js/angular/angular',
+        'angular-ui-router': '/static/js/angular-ui-router/release/angular-ui-router',
+        /*require ui-bootstrap with the embeded template [http://angular-ui.github.io/bootstrap/]*/
+        'ui-bootstrap-tpls': '/static/js/angular-bootstrap/ui-bootstrap-tpls',
+        /*require jquery*/
+        'jquery': '/static/js/jquery/dist/jquery',
+        /*require lodash library [http://lodash.com/docs]*/
+        'lodash': '/static/js/lodash/dist/lodash',
+        /*require bootstrap.js to make bootstrap components work*/
+        'bootstrap': '/static/js/sass-bootstrap/dist/js/bootstrap',
+        'FileAPI': '/static/js/FileAPI/dist/FileAPI',
+        'angular-file-upload-shim': '/static/js/ng-file-upload/angular-file-upload-shim',
+        'angular-file-upload': '/static/js/ng-file-upload/angular-file-upload'
+        /*--insert code tag--do not remove*/
+    },
+    shim: {
+        'angular': { exports: 'angular', deps: ['jquery', 'angular-file-upload-shim'] },
+        'lodash': { exports: '_' },
+        'angular-ui-router': ['angular'],
+        'ui-bootstrap-tpls': ['angular'],
+        'bootstrap': ['jquery'],
+        'angular-file-upload-shim': ['FileAPI'],
+        'angular-file-upload': ['angular']
+    }/*--requirejs config copy tag--do not remove*/
+});
--- mockingbird/static/js/main.js
+++ mockingbird/static/js/main.js
@@ -1,9 +1,50 @@
-$(document).ready(function() {
-    var $html = $('html');
-    angular.module('mockingbird', [])
-    .config(['$interpolateProvider', function($interpolateProvider) {
-         $interpolateProvider.startSymbol('[[');
-         $interpolateProvider.endSymbol(']]');
-    }]);
-    angular.bootstrap($html, ['mockingbird']);
+require([
+    'angular',
+    'jquery',
+    'angular-ui-router',
+    'ui-bootstrap-tpls',
+    'lodash',
+    'bootstrap',
+    '/static/js/home/home.js'
+], function(angular) {
+    'use strict';
+
+    /*App Module*/
+    angular.element(document).ready(function () {
+        /*smart works go here*/
+        var $html = angular.element('html');
+        angular.module('webApp', [
+            'ui.router',
+            'ui.bootstrap',
+            'homeModule'
+        ]).config(['$urlRouterProvider', '$provide', '$interpolateProvider', 
+            function($urlRouterProvider, $provide, $interpolateProvider) {
+            $urlRouterProvider.otherwise('/');
+
+            /* change configure to use [[ to be the interpolation ([[2 + 2]]) */
+            $interpolateProvider.startSymbol('[[');
+            $interpolateProvider.endSymbol(']]');
+
+            /* add safeApply function for $rootScope - called by $scope.$root.safeApply(fn) */
+            $provide.decorator('$rootScope', [
+                '$delegate',
+                function($delegate) {
+                    $delegate.safeApply = function(fn) {
+                        var phase = $delegate.$$phase;
+                        if (phase === '$apply' || phase === '$digest') {
+                            if (fn && typeof fn === 'function') {
+                                fn();
+                            }
+                        } else {
+                            $delegate.$apply(fn);
+                        }
+                    };
+                    return $delegate;
+                }
+            ]);
+        }]);
+
+        /*bootstrap model*/
+        angular.bootstrap($html, ['webApp']);
+    });
 });
