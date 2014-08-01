10d18c536818b28160823b6454ba9f1b0adcec04
hung-phan <colorvisavn@gmail.com>
2014-08-01 14:13:57 +0700

    update template

--- app/src/home/home.js
+++ app/src/home/home.js
@@ -14,14 +14,25 @@ define(['angular', 'angular-ui-router'], function(angular) {
         function($scope, $location) {
             /* initialize */
             $scope.pageTitle = 'dummy';
+
+            /*recognition webkit*/
             var recognition = new webkitSpeechRecognition();
+            var recognitionInit = false;
             recognition.lang = "en-GB";
             recognition.continuous = true;
             recognition.interimResults = true;
+            /*on result return*/
             recognition.onresult = function(event) {
                 console.log(event.results[0][0].transcript);
             }
-            recognition.start();
+            $scope.recordButton = function() {
+                recognitionInit = !recognitionInit;
+                if (recognitionInit) {
+                    recognition.start();
+                } else {
+                    recognition.stop();
+                }
+            };
         }
     ]);
 });
